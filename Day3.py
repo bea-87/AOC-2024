2f10fecc4569f4c4fee143ff49b0f7a8aebb1277
bea-87 <beatrice.price@spgs.org>
2024-12-05 19:02:19 +0000

    asd

--- Day3.py
+++ Day3.py
@@ -1,12 +1,28 @@
 import re
 
+inpt = '\n'.join(open("input.txt").readlines())
 sum = 0
-inpt = '\n'.join(open("bob.txt").readlines())
+enabled = True
 
-mul = re.compile(r'mul\(\d?\d?\d,\d?\d?\d\)')
-mul = mul.findall(inpt)
-for i in mul:
-    nums = i[4:-1].split(",")
-    sum += int(nums[0])*int(nums[1])
+mul = re.compile(r'mul\(\d{1,3},\d{1,3}\)')
+do = re.compile(r'do\(\)')
+dont = re.compile(r"don't\(\)")
+instructions = re.split(r'(mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\))', inpt)
+
+#Part 1
+'''mul_matches = mul.findall(inpt)
+for match in mul_matches:
+    nums = match[4:-1].split(",")
+    sum += int(nums[0]) * int(nums[1])'''
+
+#Part 2
+for instruction in instructions:
+    if mul.match(instruction) and enabled:
+        nums = instruction[4:-1].split(",")
+        sum += int(nums[0]) * int(nums[1])
+    elif do.match(instruction):
+        enabled = True
+    elif dont.match(instruction):
+        enabled = False
 
 print(sum)
