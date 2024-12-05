c792ae02811fdcb082b0e24f9d3fd7faaf1f6791
bea-87 <beatrice.price@spgs.org>
2024-12-05 18:35:48 +0000

    asd

--- Day2.py
+++ Day2.py
@@ -1,15 +1,11 @@
 safeValues = 0
 
 def isSafe(nums):
-    safe = 1
-    indexNotSafe = 0
     isAscending = 1 if int(nums[1]) - int(nums[0]) > 0 else -1
     for i in range(len(nums)-1):
         if not ((isAscending*3) >= int(nums[i+1]) - int(nums[i]) >= isAscending or (isAscending*3) <= int(nums[i+1]) - int(nums[i]) <= isAscending) :
-            safe = 0
-            indexNotSafe = i
-            break
-    return [safe, indexNotSafe]
+            return (0, i)
+    return (1, 0)
 
 #Part 1
 with open('bob.txt') as f:
@@ -20,19 +16,16 @@ with open('bob.txt') as f:
 print(safeValues)
 
 #Part 2
-'''with open('bob.txt') as f:
+with open('bob.txt') as f:
     for line in f:
         nums = line.split(" ")
         if isSafe(nums)[0]:
             safeValues += 1
         else:
             indexNotSafe = isSafe(nums)[1]
-            print(nums)
-            print(indexNotSafe)
-            print(nums[:indexNotSafe] + nums[indexNotSafe+1:])
             if (isSafe(nums[:indexNotSafe] + nums[indexNotSafe+1:])[0]) or (isSafe((nums[:indexNotSafe+1] + nums[indexNotSafe+2:]))[0]) or (isSafe((nums[:indexNotSafe-1] + nums[indexNotSafe:]))[0]):
                 safeValues += 1
 
 
 
-print("total", safeValues)'''
+print("total", safeValues)
--- Day5.py
+++ Day5.py
@@ -4,6 +4,8 @@ orders = []
 pageOrder = []
 sum = 0
 wrongOrders = []
+
+
 with open('bob.txt') as f:
     rules = f.read().split("\n\n")
     for i in rules[1].split("\n"):
@@ -14,11 +16,11 @@ with open('bob.txt') as f:
 
 def checkPrinter(pages):
     for i in range(len(pages)):
-        for j in range(1, len(pages)):
+        for j in range(len(pages)):
             if str(pages[i])+"|"+str(pages[j]) in rules:
                 if j<i:
                     return [False, i, j] 
-    return True
+    return [True, 0, 0]
 
 #Part 1            
 '''for k in orders:
@@ -28,16 +30,24 @@ def checkPrinter(pages):
 
 #Part 2 
 def reshuffle(pages):
-    p    
+    while True:
+        result = checkPrinter(pages)
+        if result[0]:
+            return pages
+        swapA, swapB = result[1], result[2]
+        pages[swapA], pages[swapB] = pages[swapB], pages[swapA]
+        
 
 for k in orders:
     if not checkPrinter(k)[0]:
         wrongOrders.append(k)
 
 for k in wrongOrders:
-    reshuffle(k)
+    k = reshuffle(k)
+    print(k)
     sum += int(k[int(len(k)/2-.05)])
-    print(sum)
+
+print(sum)
 
 
 
