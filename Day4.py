1bf588ab92a5476be2e1f8d99d7c4373b76b4994
bea-87 <beatrice.price@spgs.org>
2024-12-05 21:10:39 +0000

    asd

--- Day4.py
+++ Day4.py
@@ -1,66 +1,38 @@
-with open('bob.txt') as f:
+import numpy as np
+from scipy.ndimage import rotate
+with open('input.txt') as f:
    lines = f.read().split('\n')
 
-sum = 0
+sum = 0 
 
-'''for i in range(len(lines)):
-   for j in range(len(lines[i])):
-        if lines[i][j] == "X":
-            try:
-                if lines[i][j+1] + lines[i][j+2] + lines[i][j+3]== "MAS" and j+3 < len(lines[i]):
-                    sum += 1
-                    print(i, j, 0)
-            except:
-                print("")
-            try:
-                if lines[i][j-1] + lines[i][j-2] + lines[i][j-3]== "MAS" and j-3 >= 0:
-                    sum += 1
-                    print(i, j, 1)
-            except:
-                print("")
-            try:
-                if lines[i+1][j] + lines[i+2][j] + lines[i+3][j] == "MAS" and i+3 < len(lines):
-                    sum += 1
-                    print(i, j, 2)
-            except:
-                print("")
-            try:
-                if lines[i-1][j] + lines[i-2][j] + lines[i-3][j] == "MAS" and i-3 >= 0:
-                    sum += 1
-                    print(i, j, 3)
-            except:
-                print("")
-            try:
-                if lines[i+1][j+1] + lines[i+2][j+2] + lines[i+3][j+3]== "MAS" and j+3 < len(lines[i]) and i+3 < len(lines):
-                    sum += 1
-                    print(i, j, 4)
-            except:
-                print("")
-            try:
-                if lines[i+1][j-1] + lines[i+2][j-2] + lines[i+3][j-3]== "MAS" and j-3 >= 0 and i+3 < len(lines):
-                    sum += 1
-                    print(i, j, 5)
-            except:
-                print("")
-            try:
-                if lines[i-1][j+1] + lines[i-2][j+2] + lines[i-3][j+3]== "MAS" and j+3 < len(lines[i]) and i-3 >= 0:
-                    sum += 1
-                    print(i, j, 6)
-            except:
-                print("")
-            try:
-                if lines[i-1][j-1] + lines[i-2][j-2] + lines[i-3][j-3]== "MAS" and j-3 >= 0 and i-3 >= 0:
-                    sum += 1
-                    print(i, j, 7)
-            except:
-                print("")'''
+#Part 1
+for i in range(len(lines)):
+    lines[i] = list(lines[i])
+lines = np.array(lines)
 
-for i in range(1, len(lines)-1):
+def countDiagonals(lines):
+    count = 0
+    for i in range(len(lines)-3):
+        for j in range(len(lines)-3):
+                if lines[i][j] + lines[i+1][j+1] + lines[i+2][j+2] + lines[i+3][j+3]== "XMAS":
+                    count += 1
+    return count
+
+for p in range(4):
+    print(sum)
+    for j in lines:
+        sum += ''.join(j).count("XMAS")
+    print(sum)
+    sum += countDiagonals(lines)
+    lines = np.rot90(lines, k=1)
+
+
+
+#Part 2
+'''for i in range(1, len(lines)-1):
    for j in range(1, len(lines[i])-1):
         if lines[i][j] == "A":
-            try:
-                if ((lines[i-1][j-1] == "M" and lines[i+1][j+1] == "S") or (lines[i-1][j-1] == "S" and lines[i+1][j+1] == "M")) and ((lines[i-1][j+1] == "M" and lines[i+1][j-1] == "S") or (lines[i-1][j+1] == "S" and lines[i+1][j-1] == "M")):
-                    sum += 1
-            except:
-                print()
+            if ((lines[i-1][j-1] == "M" and lines[i+1][j+1] == "S") or (lines[i-1][j-1] == "S" and lines[i+1][j+1] == "M")) and ((lines[i-1][j+1] == "M" and lines[i+1][j-1] == "S") or (lines[i-1][j+1] == "S" and lines[i+1][j-1] == "M")):
+                sum += 1
+'''
 print(sum)
--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-lines = [["g", "j", "k"], ["l", "h", "w"], ["r", "t", "t"]]
-print(lines[0:3])
-print(lines[0][0:3])
+import numpy as np
+from scipy.ndimage import rotate
 
+# Define the list of lists
+lines = [["g", "j", "k"], ["l", "h", "w"], ["r", "t", "t"]]
+
+# Convert to a NumPy array
+array = np.array(lines)
+
+# Rotate by 90 degrees counterclockwise using numpy's rot90 function
+rotated_array = np.rot90(array, k=1)
+
+# Print the rotated array
+print(rotated_array)
 
