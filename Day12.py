a4ff9826af1b4d4bd74b8bc23c20f0ea009c72a8
bea-87 <beatrice.price@spgs.org>
2024-12-30 12:34:07 +0000

    Day 12 update

--- Day12.py
+++ Day12.py
@@ -9,18 +9,19 @@ numOfRows = len(lines)
 numOfCols = len(lines[0])
 
 def findTotalRegion(curRegion, spot, letter):
+    newRegion = list(curRegion)
     if lines[spot[0]][spot[1]] == letter:
-        curRegion.append(spot)
+        newRegion.append(spot)
         visited.append((spot[0], spot[1]))
         if (spot[0] +1, spot[1]) not in visited and spot[0]+1 < numOfRows:
-            curRegion = findTotalRegion(curRegion, (spot[0] + 1, spot[1]), letter)
+            newRegion = findTotalRegion(newRegion, (spot[0] + 1, spot[1]), letter)
         if (spot[0] -1, spot[1]) not in visited and spot[0]-1 >= 0:
-            curRegion = findTotalRegion(curRegion, (spot[0] - 1, spot[1]), letter)
+            newRegion = findTotalRegion(newRegion, (spot[0] - 1, spot[1]), letter)
         if (spot[0], spot[1]+1) not in visited and spot[1]+1 < numOfCols:
-            curRegion = findTotalRegion(curRegion, (spot[0], spot[1]+1), letter)
+            newRegion = findTotalRegion(newRegion, (spot[0], spot[1]+1), letter)
         if (spot[0], spot[1]-1) not in visited and spot[1]-1 >= 0:
-            curRegion = findTotalRegion(curRegion, (spot[0], spot[1]-1), letter)
-    return curRegion
+            newRegion = findTotalRegion(newRegion, (spot[0], spot[1]-1), letter)
+    return newRegion
 
 def findPeri(locations):
     peri = 0
@@ -37,25 +38,38 @@ def findPeri(locations):
 
 def findSides(locations):
     sides = 0
-
+    for i in locations:
+        x, y = i
+        orthogonal = ((x-1, y), (x, y+1), (x+1, y), (x, y-1))
+        diagonals = {(x-1, y-1), (x+1, y-1), (x-1, y+1), (x+1, y+1)}
+        orthogonalInLocations = set(orthogonal) & locations
+        diagonalsNotInLocations = diagonals - locations
+        if len(orthogonalInLocations) == 2 and ((orthogonal[0] in orthogonalInLocations) != (orthogonal[2] in orthogonalInLocations)):
+            sides += 1
+        elif len(orthogonalInLocations) == 1:
+            sides += 2
+        elif len(orthogonalInLocations) == 0:
+            sides += 4
+        for j in diagonalsNotInLocations:
+            if (j[0], y) in orthogonalInLocations and (x, j[1]) in orthogonalInLocations:
+                sides +=1
     return sides
 
 for i in range(numOfRows):
     for j in range(numOfCols):
         if (i, j) not in visited:
-            areas.append(findTotalRegion([], (i, j), lines[i][j]))
+            areas.append(set(findTotalRegion([], (i, j), lines[i][j])))
             perimeters.append(findPeri(areas[-1]))
             sides.append(findSides(areas[-1]))
 
 #Part 1
-totPrice = 0
-for i in range(len(areas)):
-    totPrice += len(areas[i])*perimeters[i]
-print(totPrice)
+# totPrice = 0
+# for i in range(len(areas)):
+#     totPrice += len(areas[i])*perimeters[i]
+# print(totPrice)
 
 #Part 2
 totPrice = 0
 for i in range(len(areas)):
     totPrice += len(areas[i])*sides[i]
-
 print(totPrice)
